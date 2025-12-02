c4cbda8f9ee4a9291ae90a2a7cb32a647ea0c59a
CrimsonCompiler <toisiftasrik@gmail.com>
2025-12-02 21:10:33 +0600

    data_type added

---
+++ data_type.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a = 10;
+    float b = 10.5;
+    char c = 'a';
+    double d = 10.5;
+    long long int e = 1000000000000000000;
+    
+    printf("The value of a is %d\n", a);
+    printf("The value of b is %f\n", b);
+    printf("The value of c is %c\n", c);
+    printf("The value of d is %lf\n", d);
+    printf("The value of e is %lld\n", e);
+    return 0;
+}
